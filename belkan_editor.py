2231e1c20d6b579540771a36fdab4920d1b9802e
Juan Luis <jlsuarezdiaz@ugr.es>
2024-04-04 12:57:33 +0200

    Added rotations, symmetries, resizes and fill.

--- belkan_editor.py
+++ belkan_editor.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, simpledialog, messagebox
 
 # Define the colors
 COLORS = {
@@ -69,6 +69,21 @@ class MatrixEditor(tk.Tk):
         self.save_button = tk.Button(self, text="Save", command=self.save_map)
         self.save_button.pack()
 
+        self.rotate_button = tk.Button(self, text="Rotate 90°", command=self.rotate_matrix)
+        self.rotate_button.pack(side=tk.RIGHT)
+        
+        self.vertical_symmetry_button = tk.Button(self, text="Vertical Symmetry", command=self.vertical_symmetry)
+        self.vertical_symmetry_button.pack(side=tk.RIGHT)
+        
+        self.horizontal_symmetry_button = tk.Button(self, text="Horizontal Symmetry", command=self.horizontal_symmetry)
+        self.horizontal_symmetry_button.pack(side=tk.RIGHT)
+
+        self.resize_button = tk.Button(self, text="Resize", command=self.resize_map)
+        self.resize_button.pack(side=tk.RIGHT)
+
+        self.fill_with_black_button = tk.Button(self, text="Fill with borders", command=self.fill_with_black)
+        self.fill_with_black_button.pack(side=tk.RIGHT)
+
         # Mouse events
         self.canvas.bind('<Button-1>', self.start_drawing)
         self.canvas.bind('<B1-Motion>', self.paint_pixel)
@@ -121,6 +136,44 @@ class MatrixEditor(tk.Tk):
 
     def on_resize(self, event):
         self.draw_matrix()
+
+    def rotate_matrix(self):
+        self.matrix = list(zip(*self.matrix[::-1]))
+        self.matrix = [list(row) for row in self.matrix]
+        self.rows, self.cols = self.cols, self.rows
+        self.draw_matrix()
+
+    def vertical_symmetry(self):
+        for row in self.matrix:
+            row[:] = row[::-1]
+        self.draw_matrix()
+
+    def horizontal_symmetry(self):
+        self.matrix.reverse()
+        self.draw_matrix()
+
+    def resize_map(self):
+        new_rows = simpledialog.askinteger("Resize", "Enter number of rows:")
+        if new_rows:
+            # new_cols = simpledialog.askinteger("Resize", "Enter number of columns:")
+            new_cols = new_rows
+            if new_cols:
+                old_matrix = self.matrix
+                self.matrix = [['S'] * new_cols for _ in range(new_rows)]
+                for y in range(min(new_rows, self.rows)):
+                    for x in range(min(new_cols, self.cols)):
+                        self.matrix[y][x] = old_matrix[y][x]
+                self.rows = new_rows
+                self.cols = new_cols
+                self.draw_matrix()
+
+    def fill_with_black(self):
+        for y in range(self.rows):
+            for x in range(self.cols):
+                if y < 3 or y >= self.rows - 3 or x < 3 or x >= self.cols - 3:
+                    self.matrix[y][x] = 'P'
+        self.draw_matrix()
+
 
 def read_matrix_from_file(filename):
     matrix = []
